216b44a992500af1d9ae4c06fd927c67e8cc9640
jtesti <100090019+jtesti@users.noreply.github.com>
2022-05-17 12:12:45 +0200

    First commit

---
+++ Ejtema3part0/src/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(sumarNumeros(10, 5, 1));
+    }
+    public static int sumarNumeros(int valor, int valor2, int valor3) {
+        return valor + valor2 + valor3;
+
+    }
+
+}
